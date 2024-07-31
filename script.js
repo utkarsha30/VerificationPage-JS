24db66e98d28d37c558d5430eb6b6681fa4ed616
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2024-07-31 16:14:36 -0400

    rndomly generating 6 images

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const imageContainer = document.getElementsByClassName("flex")[0];
-let images = []
-for(let i=0;i<5;i++){
-   images[i]=document.createElement("img")
-   images[i].src=`./images/img${i+1}.jpg`;
-   images[i].alt = `images${i+1}`
-   imageContainer.appendChild(images[i]);
-}
+
+
+resetImages()
+
 function resetImages(){
-   let img = ["img1","img2","img3","img4","img5"]
-   
-   const show = document.getElementsByClassName("show")[0];
-   let arr = [1, 2, 3, 4, 5];
+   imageContainer.innerHTML = ''; 
+   let arr = ['img1', 'img2', 'img3', 'img4','img5'];
+
     
    // Pick a random index to repeat a value
    let repeatIndex = Math.floor(Math.random() * 5);
@@ -23,9 +19,18 @@ function resetImages(){
    for (let i = arr.length - 1; i > 0; i--) {
        const j = Math.floor(Math.random() * (i + 1));
        [arr[i], arr[j]] = [arr[j], arr[i]];
+       
    }
-      show.innerHTML= arr;
+  
+   showImages(arr)
    
   
+}
+function showImages(arr){
+   arr.forEach(img=>{
+  let image=document.createElement("img")
+   image.src=`./images/${img}.jpg`
+   imageContainer.appendChild(image);
+   })
 }
 
